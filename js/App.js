cff6bdea19feed963591903ff2943866e95b2ec1
Lucas <lucassantosamaral@hotmail.com>
2018-05-31 14:19:54 -0300

    Pegando a lista de repositorios

---
+++ js/App.js
@@ -0,0 +1,21 @@
+angular.module('vml', [])
+.controller('gitHubController', function($scope, $http){
+
+    $http.get("https://api.github.com/users/globocom")
+        .then(function(response){
+            var data = response.data;
+            $scope.user = data;
+            console.log(data);
+            carregarRepo();
+        });
+
+        var carregarRepo = function () {
+            $http.get($scope.user.repos_url)
+                .then(function(response){
+                    var repoData = response.data;
+                    $scope.repos = repoData;
+                    console.log(repoData);
+                });
+        }
+
+});

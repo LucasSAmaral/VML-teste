4729fa3bde877add48bf8f30aaa02e73bc3653d2
Lucas <lucassantosamaral@hotmail.com>
2018-06-04 00:13:10 -0300

    Colocando client_id e client_secret para fazer a autenticacao OAuth

--- js/Controllers.js
+++ js/Controllers.js
@@ -2,14 +2,14 @@ angular.module('vml')
 .controller('gitHubController', function($scope, $http){
 
 
-    $http.get("https://api.github.com/users/globocom/repos")
+    $http.get("https://api.github.com/users/globocom/repos?client_id=0866ff58c883857c4857&client_secret=da97443125d958c9c2dc6fdbdf0c2f3b07cc3d7e")
         .then(function(response){
             var repoData = response.data;
             $scope.repos = repoData;
         });
 
          $scope.carregaCommits = function(repoName) {
-            $http.get("https://api.github.com/repos/globocom/"+ repoName +"/commits")
+            $http.get("https://api.github.com/repos/globocom/"+ repoName +"/commits?client_id=0866ff58c883857c4857&client_secret=da97443125d958c9c2dc6fdbdf0c2f3b07cc3d7e")
                 .then(function(response){
                     var commitData = response.data;
                     $scope.commits = commitData;

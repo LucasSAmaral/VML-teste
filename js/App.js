85a8fae7c99c06b36345bd173b055cd0c8f79470
Lucas <lucassantosamaral@hotmail.com>
2018-06-05 01:10:35 -0300

    Indentando arquivos

--- js/App.js
+++ js/App.js
@@ -1,17 +1,20 @@
 angular.module('vml', ['ngRoute'])
-    .config(['$routeProvider', '$locationProvider', 
-      function($routeProvider, $locationProvider){
+    .config(['$routeProvider', '$locationProvider',
+        function ($routeProvider, $locationProvider) {
 
-        $routeProvider
-        .when('/', {
-            templateUrl: 'template/home.html',
-            controller: 'gitHubController'
-        })
-        .when('/:name', {
-            templateUrl: 'template/repositories.html',
-            controller: 'gitHubController'
-        });
+            $routeProvider
+                .when('/', {
+                    templateUrl: 'template/home.html',
+                    controller: 'gitHubController'
+                })
+                .when('/:name', {
+                    templateUrl: 'template/repositories.html',
+                    controller: 'gitHubController'
+                });
 
-        $routeProvider.otherwise({redirectTo:"/"});
+            $routeProvider.otherwise({
+                redirectTo: "/"
+            });
 
-    }])
+        }
+    ])
--- js/Controllers.js
+++ js/Controllers.js
@@ -1,34 +1,34 @@
 angular.module('vml')
-.controller('gitHubController', function($scope, $http){
+    .controller('gitHubController', function ($scope, $http) {
 
 
-    $http.get("https://api.github.com/users/globocom/repos?client_id=0866ff58c883857c4857&client_secret=da97443125d958c9c2dc6fdbdf0c2f3b07cc3d7e")
-        .then(function(response){
-            var repoData = response.data;
-            $scope.repos = repoData;
-        });
+        $http.get("https://api.github.com/users/globocom/repos?client_id=0866ff58c883857c4857&client_secret=da97443125d958c9c2dc6fdbdf0c2f3b07cc3d7e")
+            .then(function (response) {
+                var repoData = response.data;
+                $scope.repos = repoData;
+            });
 
-         $scope.carregaCommits = function(repoName) {
-            $http.get("https://api.github.com/repos/globocom/"+ repoName +"/commits?client_id=0866ff58c883857c4857&client_secret=da97443125d958c9c2dc6fdbdf0c2f3b07cc3d7e")
-                .then(function(response){
+        $scope.carregaCommits = function (repoName) {
+            $http.get("https://api.github.com/repos/globocom/" + repoName + "/commits?client_id=0866ff58c883857c4857&client_secret=da97443125d958c9c2dc6fdbdf0c2f3b07cc3d7e")
+                .then(function (response) {
                     var commitData = response.data;
                     $scope.commits = commitData;
                     $scope.commitLength = $scope.commits.length;
                 });
         }
 
-        $scope.orderByName = function(name) {
+        $scope.orderByName = function (name) {
             $scope.orderName = name;
         }
 
         $scope.indexNumber = 19;
 
-        $scope.mostrarCommits = function() {
-            if($scope.indexNumber < $scope.commitLength) {
+        $scope.mostrarCommits = function () {
+            if ($scope.indexNumber < $scope.commitLength) {
                 $scope.indexNumber = $scope.indexNumber + 20;
                 if ($scope.indexNumber >= $scope.commitLength) {
-                    $('.btn-load-commits').hide();    
+                    $('.btn-load-commits').hide();
                 }
             }
         }
-});
+    });
